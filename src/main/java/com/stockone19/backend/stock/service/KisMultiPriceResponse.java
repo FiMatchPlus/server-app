6fda507a8a186d8e0638832435833184e4afc880
NamJihyun99 <namjihyun9936@gmail.com>
2025-09-17 11:25:34 +0900

    fix: 포트폴리오 정보 현재가 데이터 연결 수정

---
+++ src/main/java/com/stockone19/backend/stock/service/KisMultiPriceResponse.java
@@ -0,0 +1,26 @@
+package com.stockone19.backend.stock.service;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public record KisMultiPriceResponse(
+        @JsonProperty("rt_cd") String rtCd,
+        @JsonProperty("msg_cd") String msgCd,
+        @JsonProperty("msg1") String msg1,
+        @JsonProperty("output") List<ResponseBodyOutput> output
+) {
+    public record ResponseBodyOutput(
+            @JsonProperty("inter_shrn_iscd") String interShrnIscd,        // 종목코드
+            @JsonProperty("inter_kor_isnm") String interKorIsnm,          // 종목 한글명
+            @JsonProperty("inter2_prpr") String inter2Prpr,               // 현재가
+            @JsonProperty("inter2_prdy_vrss") String inter2PrdyVrss,      // 전일대비
+            @JsonProperty("prdy_vrss_sign") String prdyVrssSign,          // 전일대비부호
+            @JsonProperty("prdy_ctrt") String prdyCtrt,                   // 전일대비율
+            @JsonProperty("inter2_prdy_clpr") String inter2PrdyClpr,      // 전일종가
+            @JsonProperty("inter2_oprc") String inter2Oprc,               // 시가
+            @JsonProperty("inter2_hgpr") String inter2Hgpr,               // 고가
+            @JsonProperty("inter2_lwpr") String inter2Lwpr,               // 저가
+            @JsonProperty("acml_vol") String acmlVol,                     // 누적거래량
+            @JsonProperty("acml_tr_pbmn") String acmlTrPbmn               // 누적거래대금
+    ) {}
+}
